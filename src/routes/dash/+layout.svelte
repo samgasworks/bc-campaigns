43fa8d2aa271853b38f5214d7994c878c83c4ad4
internetmilk <sam@internetmilk.com>
2023-08-22 20:39:26 -0700

    removed campaigns from menu

--- src/routes/dash/+layout.svelte
+++ src/routes/dash/+layout.svelte
@@ -36,7 +36,7 @@
 						<img class="h-16 w-auto" src="/bc-logo.png" alt="Your Company">
 					</div>
 					<div class="-my-px ml-6 flex space-x-8">
-						{#if $page.url.pathname !== 'dash/set-password'}
+						{#if $page.url.pathname !== '/dash/set-password'}
 						<a href="/dash" class="{$page.url.pathname === '/dash' ? 'border-blue-500 text-gray-900' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700'} inline-flex items-center border-b-2 px-1 pt-1">
 							Campaigns
 						</a>
