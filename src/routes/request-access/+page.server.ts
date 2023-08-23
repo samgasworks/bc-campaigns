aabdc8b5a1f2159291ae5eb00eaacc66e426ace7
internetmilk <sam@internetmilk.com>
2023-08-22 21:34:31 -0700

    added access requests

---
+++ src/routes/request-access/+page.server.ts
@@ -0,0 +1,72 @@
+import type { Actions } from './$types';
+import { error, fail, redirect } from '@sveltejs/kit';
+import { getSupabase } from '@supabase/auth-helpers-sveltekit';
+
+export const actions: Actions = {
+	default: async (event) => {
+		const { session, supabaseClient } = await getSupabase(event);
+		if (!session) {
+			// the user is not signed in
+			throw error(403, { message: 'Unauthorized' });
+		}
+
+		const { request } = event;
+		const formData = await request.formData();
+
+		const first_name = formData.get('first_name') as string;
+		const last_name = formData.get('last_name') as string;
+		const email = formData.get('email') as string | undefined;
+
+		// check and sanitize inputs
+		const cleanedFirst = first_name.replace(/[^\w\s]/gi, '');
+		const cleanedLast = last_name.replace(/[^\w\s]/gi, '');
+
+
+		if (!first_name) {
+			return fail(400, {
+				error: 'Please enter your first name',
+				values: {
+					last_name,
+					email
+				}
+			});
+		}
+
+		if (!last_name) {
+			return fail(400, {
+				error: 'Please enter your last name',
+				values: {
+					first_name,
+					email
+				}
+			});
+		}
+
+		if (!email) {
+			return fail(400, {
+				error: 'Please enter your email',
+				values: {
+					first_name,
+					last_name
+				}
+			});
+		}
+
+		const { error: supabaseError } = await supabaseClient
+			.from('requests')
+			.insert({
+				first_name: cleanedFirst,
+				last_name: cleanedLast,
+				email: email
+			});
+		
+		if (supabaseError) {
+			return fail(500, {
+				error: 'Error submitting request'
+			});
+		}
+
+		return { success: true };
+	}
+};
+
--- src/routes/request-access/+page.svelte
+++ src/routes/request-access/+page.svelte
@@ -4,38 +4,63 @@
 	import { page } from '$app/stores';
 	import { loading } from '$lib/utils';
 
+	let accessRequested = false;
+
 	const handleSubmit: SubmitFunction = () => {
 		$loading = true;
 		return async ({ result }) => {
 			$loading = false;
+			if (result.type === 'success') {
+				accessRequested = true;
+			}
 			await applyAction(result);
 		};
 	};
 </script>
 
 <svelte:head>
-	<title>Log In | BCRV</title>
+	<title>Request Access | BCRV</title>
 </svelte:head>
 
 <div class="relative flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
 	<div class="-z-10 absolute inset-0 contrast-50 brightness-125">
-		<img src="/login.jpg" alt="" class="w-full h-full object-cover" />
+		<img src="/login.jpg" alt="" class="w-full h-full object-cover hue-rotate-180" />
 	</div>
 	<div class="sm:mx-auto sm:w-full sm:max-w-md">
 		<img class="mx-auto h-20 w-auto" src="/bc-logo.png" alt="BCRV">
 		<h2 class="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-			BCRV Campaign Manager
+			Request Access
 		</h2>
 	</div>
   
 	<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
 		<div class="bg-white px-6 py-12 shadow sm:rounded-sm sm:px-12">
 			<form method="POST" use:enhance={handleSubmit} class="space-y-6">
+				{#if accessRequested}
+				<div class="p-3 rounded-sm text-center border bg-green-100 border-green-700/50 text-green-800 text-sm font-normal">
+					<p>Your request has been recieved.</p>
+					<p class="mt-2">If it is approved, you will get an email with a link to log in to your account.</p>
+				</div>
+				{:else}
 				{#if $page.form?.error}
 					<div class="mb-4 p-3 rounded-sm border bg-red-100 border-red-700/50 text-red-700 text-sm">
 						{$page.form.error}
 					</div>
 				{/if}
+				<div>
+					<label for="first_name" class="block text-sm font-medium leading-6 text-gray-900">First Name</label>
+						<div class="mt-2">
+						<input id="first_name" name="first_name" type="text" autocomplete="given-name" required class="standard-form">
+					</div>
+				</div>
+
+				<div>
+					<label for="last_name" class="block text-sm font-medium leading-6 text-gray-900">First Name</label>
+						<div class="mt-2">
+						<input id="last_name" name="last_name" type="text" autocomplete="family-name" required class="standard-form">
+					</div>
+				</div>
+
 				<div>
 					<label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
 						<div class="mt-2">
@@ -44,22 +69,16 @@
 				</div>
 		
 				<div>
-					<label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-						<div class="mt-2">
-						<input id="password" name="password" type="password" autocomplete="current-password" required class="standard-form">
-					</div>
-				</div>
-		
-				<div>
 					<button type="submit" class="full-width-button">
-						Log in
+						Request Access
 					</button>
 				</div>
+				{/if}
 			</form>
 		</div>
   
 		<p class="mt-10 p-4 bg-white/40 shadow text-center text-sm text-gray-400/75 rounded-sm">
-			<a href="/request-access" class="leading-6 text-blue-600 hover:text-blue-400">Request Access</a>
+			<a href="/login" class="leading-6 text-blue-600 hover:text-blue-400">Login</a>
 		</p>
 	</div>
 </div>
